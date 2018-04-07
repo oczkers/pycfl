21a80b8b1ebef5bc2a185060cae6d17312c68d12
Piotr Staroszczyk <piotr.staroszczyk@get24.org>
2018-04-07 13:03:05 +0200

    update algoritm (divide, floating) & add html parser

--- example.py
+++ example.py
@@ -1,27 +1,14 @@
 import re
 import requests
 import time
-from pycfl import pycfl as _cf
+from pycfl import pycfl
 
 r = requests.Session()
 
 rc = r.get("https://nordvpn.com/login").text
 open('gdown.log', 'w').write(rc)
 if 'Checking your browser before accessing' in rc:
-    host = re.search('Checking your browser before accessing</span> (.+?)\.</h1>', rc).group(1)
-    ans = re.search('.+?={".+?":([+\(\)!\[\]]+)};', rc).group(1)
-    ans = _cf(ans)
-    for i in re.findall('.+?\..+?([*+-]{1})=([+\(\)!\[\]]+);', rc):
-        if i[0] == '*':
-            ans *= _cf(i[1])
-        elif i[0] == '+':
-            ans += _cf(i[1])
-        elif i[0] == '-':
-            ans -= _cf(i[1])
-    ans = ans + len(host)
-    # print(f'ans: {ans}')
-    print('ans: %s' % ans)
-    time.sleep(5)  # 4 is enough?
+    ans = pycfl(rc)
     jschl_vc = re.search('name="jschl_vc" value="(.+?)"', rc).group(1)
     jschl_pass = re.search('name="pass" value="(.+?)"', rc).group(1)
     params = {'jschl_vc': jschl_vc,
--- pycfl.py
+++ pycfl.py
@@ -1,11 +1,13 @@
 import re
+import time
 
 
-def pycfl(s):
+def _cf(s):
     # !+[]  1
     # !![]  1
     # ![]   0
     # []    0
+    # +!![] 10
     result = ''
     # print(s)  # DEBUG
     ss = re.split('\(|\)', s)
@@ -21,4 +23,34 @@ def pycfl(s):
         s = s.replace('+!![]', '10')
         # print(s)  # DEBUG
         result += str(sum([int(i) for i in s.split('+')]))
+        # print(result)
     return int(result)
+
+
+def cf(s):
+    ss = re.split('/', s)
+    result = _cf(ss[0])
+    for i in ss[1:]:
+        # print(i)
+        result /= _cf(i)
+    # print('result: %s ' % result)
+    return result
+
+
+def pycfl(html):
+    host = re.search('Checking your browser before accessing</span> (.+?)\.</h1>', html).group(1)
+    ans = re.search('.+?={".+?":([+/\(\)!\[\]]+)};', html).group(1)
+    ans = cf(ans)
+    # print('ans: %s' % ans)
+    for i in re.findall('.+?\..+?([*+-]{1})=([+/\(\)!\[\]]+);', html):
+        if i[0] == '*':
+            ans *= cf(i[1])
+        elif i[0] == '+':
+            ans += cf(i[1])
+        elif i[0] == '-':
+            ans -= cf(i[1])
+        # elif i[0] == '/':
+        #     ans /= _cf(i[1])
+    ans = round(ans + len(host), 10)
+    time.sleep(4.5)  # 4 is enough?
+    return ans
